24dcd7b40101395d82e8c0242dc253c68b63cf7a
ChumkiChakraborty <137317244+ChumkiChakraborty@users.noreply.github.com>
2024-09-24 22:08:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+  // Adjusted to match the expected output format
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
 };
 
 function Employee(name, age, jobTitle) {
@@ -21,6 +22,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
+  // Adjusted to match the expected output format
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
 };
 
